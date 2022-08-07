682a80254cb0d4c4b9cfcb83aa0cc1c00945a10c
m4kman <30337413+m4kman@users.noreply.github.com>
2022-08-07 23:58:49 +0500

    add react router, and emotion dependencies for mui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello World</h1>
+    </div>
+  );
 }
