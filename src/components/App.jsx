e8e9b72da1d08268635236d855a686401f411831
m4kman <30337413+m4kman@users.noreply.github.com>
2022-08-13 19:45:57 +0500

    fix: content hiding under sidebar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
 import { Actors, MovieInformation, Movies, Navbar, Profile } from "./index";
 
 export default function App() {
   return (
-    <div>
+    <Box sx={{ display: "flex", height: "100%", flexWrap: "wrap" }}>
       <CssBaseline />
 
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Movies />} />
-        <Route path="/movie/:id" element={<MovieInformation />} />
-        <Route path="/actor/:id" element={<Actors />} />
-        <Route path="/profile/:id" element={<Profile />} />
-      </Routes>
-    </div>
+      <Box sx={{ flexGrow: 1, padding: "2em" }}>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actor/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </Box>
+    </Box>
   );
 }
