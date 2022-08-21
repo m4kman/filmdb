2059584088c50180a757f4833970f179828e31f4
m4kman <30337413+m4kman@users.noreply.github.com>
2022-08-21 10:54:38 +0500

    feature: implement logout functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  function logout() {
+    localStorage.clear();
+
+    window.location.href = "/";
+  }
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Typography variant="h4">My Profile</Typography>
+      <Button color="inherit" onClick={logout}>
+        Logout &nbsp; <ExitToApp />
+      </Button>
+    </Box>
+  );
 }
