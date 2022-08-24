a3a8e9f66335fb46fe1c51dedfcf4ae6aeeef6ae
m4kman <30337413+m4kman@users.noreply.github.com>
2022-08-24 23:08:45 +0500

    update: update movieinformation component to use endpoint

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+import { useGetMovieQuery } from "../../services/TMDB";
+import { Link, useParams } from "react-router-dom";
 
 export default function MovieInformation() {
-  return <div>MovieInformation</div>;
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  if (isFetching) {
+    return (
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+  if (error) {
+    return (
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Link to="/">Something went wrong. Click to go back.</Link>
+      </Box>
+    );
+  }
+  console.log(data);
+
+  return <div>Movie Information {id}</div>;
 }
